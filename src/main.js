e154008d11ee326e947117dcf6ffb2b29c26eab8
Tobias Höller <tobias.hoeller2005@gmail.com>
2026-01-31 18:41:36 +0100

    ui: implemented first 3 tasks

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, dialog, ipcMain, Menu } = require('electron');
 const path = require('path');
+const packageInfo = require('../package.json');
 
 try {
     require('electron-reloader')(module);
@@ -9,6 +10,7 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 1000, // Etwas breiter wirkt oft moderner
         height: 700,
+        title: `Marki v${packageInfo.version}`,
         webPreferences: {
             // Da preload.js im GLEICHEN Ordner wie main.js liegt (beide in src):
             preload: path.join(__dirname, 'preload.js')
@@ -20,6 +22,11 @@ function createWindow() {
     // Da index.html im GLEICHEN Ordner wie main.js liegt:
     win.loadFile(path.join(__dirname, 'index.html'));
 }
+
+// Höre auf den Kanal 'log-to-terminal'
+ipcMain.on('log-to-terminal', (event, message) => {
+    console.log("Renderer sagt:", message);
+});
 
 ipcMain.handle('dialog:openDirectory', async () => {
     const { canceled, filePaths } = await dialog.showOpenDialog({
